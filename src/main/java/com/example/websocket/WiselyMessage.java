93d5c3bf0d0f4480fe239240f039e0982ddcd291
yangfeng <1041114134@qq.com>
2019-01-15 09:57:01 +0800

    first commit

---
+++ src/main/java/com/example/websocket/WiselyMessage.java
@@ -0,0 +1,13 @@
+package com.example.websocket;
+
+public class WiselyMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/websocket/WsController.java
@@ -0,0 +1,21 @@
+package com.example.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WsController {
+
+    @Autowired
+    public SimpMessagingTemplate template;
+
+    @MessageMapping("/queue")
+    public void queuw(WiselyMessage rm) {
+            /*广播使用convertAndSendToUser方法，第一个参数为用户id，此时js中的订阅地址为
+            "/user/" + 用户Id + "/message",其中"/user"是固定的*/
+            template.convertAndSendToUser("zhangsan","/message",rm.getName());
+        }
+
+}
